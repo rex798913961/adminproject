4a099f582b542877cd41a764e9927164b0c022e4
zhangzhifeng <798913961@qq.com>
2020-03-30 15:59:00 +0800

    Rex-addemployee初始化

--- src/pages/Employee/AddEmployeeInfo/index.js
+++ src/pages/Employee/AddEmployeeInfo/index.js
@@ -1,11 +1,70 @@
 import React,{Component} from 'react'
+import {Form,Input,DatePicker,Button} from 'antd';
 
 class AddEmployeeInfo extends Component {
-    render(){
-        return(
-            <div>这是员工添加页面</div>
-        )
-    }
-}
+    state = {
+        confirmDirty: false,
+        autoCompleteResult: [],
+      }
+    
+      handleSubmit = e => {
+        e.stopPropagation()
+        console.log(e);
+         this.props.form.validateFields(
+            (err, values) => {
+                if (err) {
+                    return
+                }
+                console.log(values);
+                
+         })
+      }
+    
+      render() {
+        const { getFieldDecorator } = this.props.form;
+        const formItemLayout = {
+          labelCol: {
+            xs: { span: 24 },
+            sm: { span: 8 },
+          },
+          wrapperCol: {
+            xs: { span: 24 },
+            sm: { span: 16 },
+          },
+        };
+        return (
+          <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+            <Form.Item label="姓名">
+              <Input/>
+            </Form.Item>
+            <Form.Item label="邮箱">
+          {getFieldDecorator('email', {
+            rules: [
+              {
+                type: 'email',
+                message: 'The input is not valid E-mail!',
+              },
+              {
+                required: true,
+                message: 'Please input your E-mail!',
+              },
+            ],
+          })(<Input />)}
+        </Form.Item>
+            
+            <Form.Item label='出生日期' >
+                <DatePicker onChange={(e)=>{console.log(e._d)}} />
+            </Form.Item>
 
-export default AddEmployeeInfo
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                添加
+              </Button>
+            </Form.Item>
+          </Form>
+        );
+      }
+}
+const AddEmployee = Form.create({ name: 'AddEmployee' })(AddEmployeeInfo)
+export default AddEmployee
