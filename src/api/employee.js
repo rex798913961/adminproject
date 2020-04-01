0712ac5b35d769e78fff07530ab4513191bffb0f
zhangzhifeng <798913961@qq.com>
2020-04-01 11:21:01 +0800

    Rex

--- src/api/employee.js
+++ src/api/employee.js
@@ -11,6 +11,16 @@ class employee {
         let url ='/panda/employee/employeeadd'
         return axios.post(url,payload)
     }
+    // 删除员工
+    del(_id){
+        let url = '/panda/employee/delemployee'
+        return axios.delete(url,{data:{_id}})
+    }
+    // 更新员工信息
+    update(_id,obj){
+        let url = '/panda/employee/updateemployee'
+        return axios.put(url,{_id,obj})
+    }
 }
 
 export default new employee()
--- src/pages/Employee/EmployeeInfo/index.js
+++ src/pages/Employee/EmployeeInfo/index.js
@@ -1,7 +1,6 @@
 import React,{Component} from 'react';
 import api from '../../../api/employee'
-import { Table, Divider, Tag } from 'antd';
-
+import { Table,Popconfirm, message} from 'antd';
 
 
 class Employee extends Component{
@@ -46,26 +45,50 @@ class Employee extends Component{
                     key: 'salary',
                 },
                 {
-                  title: 'Action',
+                  title: '操作',
                   key: 'action',
                   render: (text, record) => (
-                    <span>
-                      <a>Invite {record.name}</a>
-                      <Divider type="vertical" />
-                      <a>Delete</a>
-                    </span>
+                      <Popconfirm
+                        title="确定要删除这个员工的信息吗？"
+                        onConfirm={()=>{
+                          this.del(record._id)
+
+                        }}
+                        onCancel={()=>{
+                          message.error('取消删除');
+                        }}
+                        okText="Yes"
+                        cancelText="No"
+                      >
+                        <a href="#">删除</a>
+                      </Popconfirm>
                   ),
                 },
               ]
         };
       }
     
+      del=async (_id)=>{
+        // 获取id 掉接口 刷新界面
+        console.log('删除',_id)
+        let result =await api.del(_id)
+        console.log(result);
+        
+        // 根据结果进行
+        // if(result.code !==0){ return false }
+        this.refreshList() 
+      }
+      //刷新列表数据
+   refreshList=async ()=>{
+    let result= await api.findList()
+    this.setState({data:result.msg})
+   }  
     async componentDidMount(){
         // 请求员工列表
         let result= await api.findList()
         this.setState({data:result.msg})
 
-        console.log(this);
+        console.log(this.state.data);
         // let birthdate=this.state.data.msg[8].birthdate
         // let d=new Date(birthdate)
         // console.log( this.date(d));
