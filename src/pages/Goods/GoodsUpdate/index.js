ff9065df1a8d0c8f3cc1b5b12732a6535beb5d9a
ouguankaile <1475419108@qq.com>
2020-04-02 10:09:30 +0800

    Goods-yitian

--- src/pages/Goods/GoodsUpdate/index.js
+++ src/pages/Goods/GoodsUpdate/index.js
@@ -38,7 +38,7 @@ class GoodsAdd extends Component{
         if(types.indexOf(type.split('/')[1])===-1){return message.warning('只允许图片格式为jpg,png,gif,jpeg,svg')}
         let formdata = new FormData()
         formdata.append('pic',file)
-        let {err,msg,path}= await (await goodsApi.uploadImg(formdata)).data
+        let {err,msg,path}= await (await goodsApi.uploadImg(formdata))
         if(err){return message.error(msg)}
         this.setState({path})
         
